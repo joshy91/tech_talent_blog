d958f075bc580699ad812014fee761e37f7420e9
Javier Ruedas <jruedas@twc.com>
2018-05-18 16:59:20 -0500

    Attempt to resolve comment issue

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
 	belongs_to :post
+<<<<<<< HEAD
 	belongs_to :user
+=======
+>>>>>>> cf59bc5... add comments
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
 	has_many :comments
+<<<<<<< HEAD
 	belongs_to :user
+=======
+>>>>>>> cf59bc5... add comments
 end
