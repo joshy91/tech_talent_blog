0dbcebf854e09ff68ca5185f99150e9c922b99bd
joshy91 <joshua.davidsonsga@gmail.com>
2018-05-24 14:49:24 -0500

    Non working comments w/ username

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,7 +62,7 @@ class PostsController < ApplicationController
     end
   end
 
-  private
+ private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
