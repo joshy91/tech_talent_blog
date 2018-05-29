8b3aae8d68d762e4bffd5d9abf32cfc7650830e2
joshy91 <joshua.davidsonsga@gmail.com>
2018-05-29 02:35:14 -0500

    update to class state

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,4 @@
 class Comment < ApplicationRecord
 	belongs_to :post
-<<<<<<< HEAD
 	belongs_to :user
-=======
->>>>>>> cf59bc5... add comments
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ApplicationRecord
 	has_many :comments
-<<<<<<< HEAD
 	belongs_to :user
-=======
->>>>>>> cf59bc5... add comments
 end
